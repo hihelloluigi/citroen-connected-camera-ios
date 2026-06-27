3ec728ef0a96d72a9b0a0360427a4a2c2f18ad4b
Luigi Aiello <luigiaiello97@gmail.com>
2026-06-27 14:51:51 +0200

    chore: scaffold repo and empty VIRBKit package

---
+++ Packages/VIRBKit/Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "VIRBKit",
+    platforms: [.iOS(.v17), .macOS(.v14)],
+    products: [
+        .library(name: "VIRBKit", targets: ["VIRBKit"])
+    ],
+    targets: [
+        .target(name: "VIRBKit"),
+        .testTarget(name: "VIRBKitTests", dependencies: ["VIRBKit"], resources: [.copy("Fixtures")])
+    ]
+)
