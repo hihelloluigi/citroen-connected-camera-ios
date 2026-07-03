09e75fc4ca11127937b802d6f6c122ad27333393
Luigi Aiello <luigiaiello97@gmail.com>
2026-07-03 11:30:28 +0200

    feat(appcore): add generate-once phone-id store

---
+++ Packages/AppCore/Sources/AppCore/PhoneId/SecureStore.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+/// Minimal key/value secret storage. The app backs this with the Keychain; tests use a fake.
+public protocol SecureStore: Sendable {
+    func data(forKey key: String) -> Data?
+    func set(_ data: Data, forKey key: String) throws
+}
